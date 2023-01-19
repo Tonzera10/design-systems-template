f8f39c3a7e31be191a79daf6256ab03ac9867ec9
Everton Mello <everton.mello.281@gmail.com>
2023-01-18 22:56:44 -0300

    aula ok

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+import { ChakraProvider } from '@chakra-ui/react'
+import ProductSimple from "./components/ProductSimple";
+import Button from "./components/ColorModeToggle";
+import ColorModeToggle from "./components/ColorModeToggle";
+import ProductAddToCart from './components/ProductSimple';
+import NewCard from './components/NewCard';
+import {toys} from './data'
 
 export default function App() {
-  const [users, setUsers] = useState([]);
+  
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  // useEffect(() => {
+  //   fetchUsers();
+  // }, []);
 
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
+  // const fetchUsers = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       "https://jsonplaceholder.typicode.com/users"
+  //     );
 
-      setUsers(response.data);
-    } catch (error) {
-      console.log("Erro ao buscar usuários");
-      console.log(error);
-    }
-  };
+  //     setUsers(response.data);
+  //   } catch (error) {
+  //     console.log("Erro ao buscar usuários");
+  //     console.log(error);
+  //   }
+  // };
 
   return (
-    <>
-      <h1>Me apague quando for iniciar!</h1>
-      <p>Chame o Card aqui!</p>
-    </>
+    <ChakraProvider>
+      <ColorModeToggle/>
+      {toys.map((user)=>{
+        return(
+        <ProductSimple
+        key={user.id}
+        user={user}
+        />
+        )
+      })}
+    </ChakraProvider>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const toys = [
+    {
+      id: 1,
+      nome: 'Naruto',
+      cla: 'Uzumaki',
+      imagem: 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQ9UhEJvNeKHA5FWsur-N1EiMSf8fh7lo9lVESNo-B0YCbPP6DkbImq3nqJfcB8K4W2oIH7MklKjK2kD9nGy8Yy_GHC3Cgcezi_Qp5-zqIkVWMjdVoMALF1mA&usqp=CAE',
+      preco: 100,
+      precoDesc: 50
+    },
+    {
+      id: 2,
+      nome: 'Sasuke',
+      cla: 'Uchiha',
+      imagem: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQOYYew16F4rFGO8usGufjeqyW8fO2FUuoXhJwa0sSQMvkvC7PpGjWK0DJFlc7YDWii1ZIQTFO-Bo2u5b6FzXLWqg6NgftQgG9NKlaEgRFBZKjj9pHC8ZprfA&usqp=CAE',
+      preco: 100,
+      precoDesc: 50
+    },
+    {
+      id: 3,
+      nome: 'Sakura',
+      cla: 'Haruno',
+      imagem: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQvfbUODy2q5KCv04X-EXajK33t0Ke4B1DH45AqLsO52xSliqq45M1m3S3pnVSLFcRe9sSeR77yuUuEiDJRpCcphKcXIGCDua6NFrCic81JnxkE-UmoMcyD&usqp=CAE',
+      preco: 100,
+      precoDesc: 50
+    }
+  ];
